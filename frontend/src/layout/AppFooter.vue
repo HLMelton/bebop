a1169ea40ad811ae996572c57aca00db9ef9ac4e
HLMelton <Hunterlmelton@gmail.com>
2025-12-15 15:46:57 -0800

    frontend

---
+++ frontend/src/layout/AppFooter.vue
@@ -0,0 +1,7 @@
+<script setup>
+import URL from "@/components/URL.vue";
+</script>
+
+<template>
+    <!-- <div class="layout-footer font-bold text-lg"></div> -->
+</template>
---
+++ frontend/src/views/pages/Wails.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Greet from '@/components/Greet.vue'
+</script>
+
+<template>
+    <div>
+        <div class="grid grid-cols-12 h-fit min-h-fit w-fit min-w-fit gap-4">
+            <div class="col-span-12">
+                <!-- <Greet/> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+</style>
